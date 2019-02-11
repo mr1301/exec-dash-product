319d503cc0d5d09894ae92edda3396bd325f8785
mr1301 <mr1301@georgetown.edu>
2019-02-11 18:52:03 -0500

    summing totals & alt solutions

--- monthly-sales.py
+++ monthly-sales.py
@@ -1,23 +1,31 @@
 # monthly_sales.py
 
 # TODO: import some modules and/or packages here
+import csv
 
-# TODO: write some Python code here to produce the desired functionality...
+# TODO use input to pick different folders maybe?
+csv_file_path = ("201710.csv")
 
-print("-----------------------")
-print("MONTH: March 2018")
+sales_data = []
+with open(csv_file_path, "r") as csv_file:  # "r" means "open the file for reading"
+    reader = csv.DictReader(csv_file)  # assuming your CSV has headers
+    # reader = csv.reader(csv_file) # if your CSV doesn't have headers
+    for row in reader:
+        #d = dict(row)
+        d = {"date": row["date"], "product": row["product"], "unit price": float(row["unit price"]),"units sold": int(row["units sold"]),"sales price": float(row["sales price"])}
+        #print(d["product"], d["unit price"])
+        sales_data.append(d)
 
-print("-----------------------")
-print("CRUNCHING THE DATA...")
+sales_prices = [float(row["sales price"]) for row in sales_data]
+total_sales = sum(sales_prices)
+print(total_sales)
 
-print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+unique_list = list(set([p["product"] for p in sales_data]))
+#print(unique_list)
 
-print("-----------------------")
-print("TOP SELLING PRODUCTS:")
-print("  1) Button-Down Shirt: $6,960.35")
-print("  2) Super Soft Hoodie: $1,875.00")
-print("  3) etc.")
-
-print("-----------------------")
-print("VISUALIZING THE DATA...")
+top_sellers = []
+for sales_item in unique_list:
+  item_monthly_sales = sum([float(row["sales price"]) for row in sales_data])
+  d = {"product": sales_item, "monthly_sales":item_monthly_sales}
+  top_sellers.append(d)
+  print(d["product"], d["monthly_sales"])
