b392f715269ebcd380233485824ee5949ee15e86
mr1301 <mr1301@georgetown.edu>
2019-02-15 17:40:39 -0500

    sort & find type
    Adatpted from TA

--- monthly-sales.py
+++ monthly-sales.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from operator import itemgetter
+
 # monthly_sales.py
 
 # TODO: import some modules and/or packages here
@@ -19,8 +21,7 @@ with open(csv_file_path, "r") as csv_file:  # "r" means "open the file for readi
 
 
 def to_usd(my_price):
-  return f"${my_price:,.2f}"
-
+  return "${0:,.2f}".format(my_price)
 
 #************************** Above Import Data Using CSV***********************************
 month = "OCTOBER"  # TODO: get from file name or date values
@@ -32,11 +33,11 @@ total_sales = sum(sales_prices)
 #print(total_sales)
 
 print("-------------------------------")
-print(f"MONTH: {month} {year}")
+print("MONTH: {month} {year}")
 print("-------------------------------")
 print("CRUNCHING THE DATA...")
 print("-------------------------------")
-print("TOTAL MONTHLY SALES:", str(to_usd(total_sales)))
+print("TOTAL MONTHLY SALES:" + to_usd(total_sales))
 
 #*****************************Above Calc & Print Total Price********************************
 print("-------------------------------")
@@ -45,25 +46,34 @@ print("TOP SELLING PRODUCTS:")
 unique_list = list(set([p["product"] for p in sales_data]))
 #print(unique_list)
 
-top_sellers = []
+
+product_sales = []
 for sales_item in unique_list:
   item_monthly_sales = sum([float(row["sales price"]) for row in sales_data if row["product"] == sales_item])
   d = {"product": sales_item, "monthly_sales":to_usd(item_monthly_sales)}
-  top_sellers.append(d)
-  print("+", d["product"], d["monthly_sales"])
+  product_sales.append(d)
+  #print("+ " + d["product"] + " " + d["monthly_sales"])
+
+sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
+top_sellers = sorted_product_sales[0:3]
+#print(type(top_sellers))
+
+
 
 #*******************************Above List of Products & Sales***********************************
+
+breakpoint()
 
 print("-------------------------------")
 print("VISUALIZING THE DATA...")
 item = []
 sales = []
 
-for p in top_sellers:
+for p in product_sales:
     item.append(p["product"])
     sales.append(p["monthly_sales"])
 
 plt.bar(item,sales)
 plt.ylabel("sales")
 plt.xlabel("item")
-plt.show()
+#plt.show()
